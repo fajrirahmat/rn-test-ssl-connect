de0a5e1165b5069ffb8ac3e3b06f57b62533ec09
fajrirahmat <fajri.rahmat123@gmail.com>
2019-06-21 14:41:55 +0700

    Update Constant.js

--- Constant.js
+++ Constant.js
@@ -1,8 +1,7 @@
 export default {
-  baseUrl: 'https://api-dev-1.daya-dms.id',
+  baseUrl: 'https://api-*************',
   commonHeaders: {
     'Content-Type': 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbiJ9.5BG9SEVOGo_xRhtT8IkyoSy60kPg8HM9Vpvb0TdNew4'
+    Authorization: 'Bearer **********************************'
   }
 };
